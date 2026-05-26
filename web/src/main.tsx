6eb67f38008896db4ccc258fe350e51240743f5b
arcenioadames <arcenio.1701625174@ucaldas.edu.co>
2026-05-25 19:28:26 -0500

    feat(analytics-dashboard-ui): sync dashboard web changes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App(): React.JSX.Element {
-  return <div>EcoTrack Web</div>;
-}
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./routing/AppRouter";
+import { AuthProvider } from "./services/auth-context";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) {
@@ -12,7 +11,13 @@ if (!rootEl) {
 
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 );
+
+
 
